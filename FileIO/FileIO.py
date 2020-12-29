f4a3135b9c889e3e3197824294d1f2b21f75ffee
Shivam Singla <singl.shivam01@yahoo.com>
2020-12-29 01:37:31 -0400

    Added FileIO and Logger directories and will be modified for this project specifically.

---
+++ FileIO/FileIO.py
@@ -0,0 +1,45 @@
+class FileIO:
+    def __init__(self, filename, accessMode):
+        self.file = open(filename, accessMode)
+
+    def changeAccessMode(self, accessMode):
+        self.file.close()
+        self.file = open(self.file.name, accessMode)
+        
+    def read(self):
+        if not (self.file.readable()):
+            self.changeAccessMode("r")
+        return self.file.read()
+
+    def readLine(self):
+        if not (self.file.readable()):
+            self.changeAccessMode("r")
+        data = self.file.readline()
+        return data
+
+    def write(self, data):
+        if not (self.file.writable()):
+            self.changeAccessMode("w")
+        self.file.write(data)
+
+    def writeLine(self, data):
+        if ((self.file.mode != "a") or (self.file.mode != "a+") or (self.file.mode != "ab") or (self.file.mode != "ab+")):
+            self.changeAccessMode("a")
+        data = "\n" + data
+        self.file.write(data)
+
+    def __del__(self):
+        self.file.close()
+
+def test():
+    data = "Opens a file for appending.\nThe file pointer is at the end of the file if the file exists." + "\nThat is, the file is in the append mode. If the file does not exist, it creates a new file for writing."
+
+    f = FileIO("test1.txt", "w")
+    f.write(data)
+    f.writeLine("\n\nMore text added")
+
+    f.writeLine("\n\nMore text added to it")
+    print (f.readLine(), end='')
+    print (f.readLine(), end='')
+    print (f.read(), end='')
+    del f
---
+++ Logger/Logger.py
@@ -0,0 +1,58 @@
+import FileIO.FileIO as FileIO
+import os
+import datetime as dt
+
+class Logger:
+    def __init__(self, msg):
+        if (os.path.isfile("Debug/log.txt")):
+            os.remove("Debug/log.txt")
+        self.file = FileIO.FileIO("Debug/log.txt", "a")
+        self.file.write(msg + "\n" + self.datetime() + "\n")
+
+    def datetime(self):
+        return str(dt.datetime.now().strftime("%d/%m/%Y %I:%M:%S %p"))
+
+    def info(self, msg, arg0=None, arg1=None, arg2=None, arg3=None, arg4=None, arg5=None, arg6=None,
+             arg7=None, arg8=None, arg9=None, arg10=None, arg11=None, arg12=None, arg13=None, arg14=None, arg15=None):
+        args = [arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15]
+        
+        for arg in args:
+            if (arg != None):
+                msg = msg + " " + str(arg)
+        self.file.writeLine(self.datetime() + " Info: " + msg)
+
+    def warning(self, msg, arg0=None, arg1=None, arg2=None, arg3=None, arg4=None, arg5=None, arg6=None,
+             arg7=None, arg8=None, arg9=None, arg10=None, arg11=None, arg12=None, arg13=None, arg14=None, arg15=None):
+        args = [arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15]
+        
+        for arg in args:
+            if (arg != None):
+                msg = msg + " " + str(arg)
+        self.file.writeLine(self.datetime() + " Warning: " + msg)
+
+    def error(self, msg, arg0=None, arg1=None, arg2=None, arg3=None, arg4=None, arg5=None, arg6=None,
+             arg7=None, arg8=None, arg9=None, arg10=None, arg11=None, arg12=None, arg13=None, arg14=None, arg15=None):
+        args = [arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15]
+        
+        for arg in args:
+            if (arg != None):
+                msg = msg + " " + str(arg)
+        self.file.writeLine(self.datetime() + " Error: " + msg)
+
+    def __del__(self):
+        del self.file
+
+def test():
+    log = Logger("Numerical Analysis Research Thesis Log")
+
+    log.info("Numerical Analysis Logs")
+    log.info("Logger Check", 123, "and", 456)
+
+    log.warning("Test Warning")
+    log.warning("Test Warning", 123, "and", 456)
+
+    log.warning("Test Error")
+    log.warning("Test Error", 123, "and", 456)
+    del log
+
+test()
