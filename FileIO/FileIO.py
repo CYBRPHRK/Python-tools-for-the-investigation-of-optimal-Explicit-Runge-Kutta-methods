003c0b330dd12b73b2442eafeaa418d3efcd725c
Shivam Singla <singl.shivam01@yahoo.com>
2021-01-04 00:32:55 -0400

    In FileIO.py, fixed Bugs which could cause the program to throw an error and stop/crash. If a file is opened in read mode, it checks if the file or directory exists and functions accordingly. It also checks if the access mode given to open a file is valid or not and functions accordingly.

--- FileIO/FileIO.py
+++ FileIO/FileIO.py
@@ -1,30 +1,53 @@
+import os
+
 class FileIO:
     def __init__(self, filename, accessMode):
-        self.file = open(filename, accessMode)
+        if accessMode in ['r', 'rb', 'r+', 'rb+', 'w', 'w+', 'wb', 'wb+', 'a', 'a+', 'ab', 'ab+']:
+            if ((accessMode in ['r', 'rb', 'r+', 'rb+']) and not (os.path.exists(filename))):
+                print ("The given path is not a file, directory or a valid symlink.")
+            else:
+                self.file = open(filename, accessMode)
+        else:
+            print ("Invalid access mode.")
 
     def changeAccessMode(self, accessMode):
-        self.file.close()
-        self.file = open(self.file.name, accessMode)
+        if (hasattr(self, 'file')):
+            self.file.close()
+            self.file = open(self.file.name, accessMode)
+        else:
+            print ("The object is not initialized. Check the file path or the accessMode.")
         
     def read(self):
-        if not (self.file.readable()):
-            self.changeAccessMode("r")
-        return self.file.read()
+        if (hasattr(self, 'file')):
+            if not (self.file.readable()):
+                self.changeAccessMode("r")
+            return self.file.read()
+        else:
+            print ("The object is not initialized. Check the file path or the accessMode.")
+            return "Nothing to read"
 
     def readLine(self):
-        if not (self.file.readable()):
-            self.changeAccessMode("r")
-        data = self.file.readline()
-        return data
+        if (hasattr(self, 'file')):
+            if not (self.file.readable()):
+                self.changeAccessMode("r")
+            data = self.file.readline()
+            return data
+        else:
+            print ("The object is not initialized. Check the file path or the accessMode.")
+            return "Nothing to read"
 
     def write(self, data, end='\n'):
-        if not (self.file.writable()):
-            self.changeAccessMode("a")
-        data = data + end
-        self.file.write(data)
+        if (hasattr(self, 'file')):
+            if not (self.file.writable()):
+                self.changeAccessMode("a")
+            data = data + end
+            self.file.write(data)
+        else:
+            print ("The object is not initialized. Check the file path or the accessMode.")
 
     def __del__(self):
-        self.file.close()
+        if (hasattr(self, 'file')):
+            self.file.close()
 
 def test():
     data = "Opens a file for appending.\nThe file pointer is at the end of the file if the file exists." + "\nThat is, the file is in the append mode. If the file does not exist, it creates a new file for writing."
