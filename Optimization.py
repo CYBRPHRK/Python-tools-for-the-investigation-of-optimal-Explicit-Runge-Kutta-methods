63115a6bd3d53058c46f13b5957cf7b7077ce49a
Shivam Singla <singl.shivam01@yahoo.com>
2021-03-04 13:25:42 -0400

    In Optimization.py, Fixed the errors in E4Eq1(), E4Eq4(), E4Eq7() and E4Eq9(). Added the remaining 4 cases of Forth Order RK method for E4. Added displayMenu() function to display options, chooseE4Case() function to choose case for E4 and initializeOptimizer() function to start the initialization on the basis of choice made. Added Logger to log all the details of the computation in log.txt.

--- Optimization.py
+++ Optimization.py
@@ -1,5 +1,6 @@
 import scipy.optimize as scope
 from math import inf
+import Logger.Logger as logger
 
 case = 0
 
@@ -9,19 +10,14 @@ def optimize(f):
     elif(f == E3):
         alpha = [0.01, 0.01]
     elif(f == E4):
-        alpha = [0.01, 0.02]
-        '''
         if (case == 1):
-            alpha = [0.1, 0.1]
+            alpha = [0.1, 0.2]
         elif ((case == 2) or (case == 3) or (case == 4) or (case == 5)):
             alpha = [0.1]
-        '''
-
-    res = scope.minimize(f, alpha)
+            
+    log.info("alpha: ", alpha)
+    res = scope.minimize(f, alpha, tol=1e-8)
     print (res)
-
-def f1(x):
-    return (x**2) + 2
 
 def E2(alpha):
     b = [1 - (1 / (2 * alpha[0])), 1 / (2 * alpha[0])]
@@ -45,6 +41,8 @@ def setValuesForThirdOrderCase1(alpha):
     a31 = (c3 * (c3 - (3 * c2) + (3 * c2 * c2))) / (c2 * ((3 * c2) - 2))
     a32 = (c3 * (c2 - c3)) / (c2 * ((3 * c2) - 2))
 
+    log.info("Coefficients selected for E3 are:")
+    log.info("c2, c3, b1, b2, b3, a31, a32", c2, c3, b1, b2, b3, a31, a32)
     return c2, c3, b1, b2, b3, a31, a32
 
 def E3Eq1(c, b, A):
@@ -116,26 +114,80 @@ def E3(alpha):
 
     return result
 
-def setValuesForFourthOrderCase1(alpha):
-    c2 = alpha[0]
-    c3 = alpha[1]
-    c4 = 1
-    a31 = (c3 * ((3 * c2) - c3 - (4 * c2 * c2))) / (2 * c2 * (1 - (2 * c2)))
-    a32 = (c3 * (c3 - c2)) / (2 * c2 * (1 - (2 * c2)))
-    a41 = (((c3 ** 2) * ((12 * c2 * c2) - (12 * c2) + 4)) - (c3 * ((12 * c2 * c2) - (15 * c2) + 5)) + ((4 * c2 * c2) - (6 * c2) + 2)) / ((2 * c2 * c3) * (3 - (4 * (c2 + c3)) + (6 * c2 * c3)))
-    a42 = (((-4 * c3 * c3) + (5 * c3) + c2 - 2) * (1 - c2)) / ((2 * c2) * (c3 - c2) * (3 - (4 * (c2 + c3)) + (6 * c2 * c3)))
-    a43 = ((1 - (2 * c2)) * (1 - c3) * (1 - c2)) / (c3 * (c3 - c2) * (3 - (4 * (c2 + c3)) + (6 * c2 * c3)))
-    b1 = (1 - (2 * (c2 + c3)) + (6 * c2 * c3)) / (12 * c2 * c3)
-    b2 = ((2 * c3) - 1) / ((12 * c2) * (c3 - c2) * (1 - c2))
-    b3 = (1 - (2 * c2)) / ((12 * c3) * (c3 - c2) * (1 - c3))
-    b4 = (3 - (4 * (c2 + c3)) + (6 * c2 * c3)) / (12 * (1 - c2) * (1 - c3))
+def setValuesForFourthOrder(alpha):
+    if (case == 1):
+        c2 = alpha[0]
+        c3 = alpha[1]
+        c4 = 1
+        a31 = (c3 * ((3 * c2) - c3 - (4 * c2 * c2))) / (2 * c2 * (1 - (2 * c2)))
+        a32 = (c3 * (c3 - c2)) / (2 * c2 * (1 - (2 * c2)))
+        a41 = (((c3 ** 2) * ((12 * c2 * c2) - (12 * c2) + 4)) - (c3 * ((12 * c2 * c2) - (15 * c2) + 5)) + ((4 * c2 * c2) - (6 * c2) + 2)) / ((2 * c2 * c3) * (3 - (4 * (c2 + c3)) + (6 * c2 * c3)))
+        a42 = (((-4 * c3 * c3) + (5 * c3) + c2 - 2) * (1 - c2)) / ((2 * c2) * (c3 - c2) * (3 - (4 * (c2 + c3)) + (6 * c2 * c3)))
+        a43 = ((1 - (2 * c2)) * (1 - c3) * (1 - c2)) / (c3 * (c3 - c2) * (3 - (4 * (c2 + c3)) + (6 * c2 * c3)))
+        b1 = (1 - (2 * (c2 + c3)) + (6 * c2 * c3)) / (12 * c2 * c3)
+        b2 = ((2 * c3) - 1) / ((12 * c2) * (c3 - c2) * (1 - c2))
+        b3 = (1 - (2 * c2)) / ((12 * c3) * (c3 - c2) * (1 - c3))
+        b4 = (3 - (4 * (c2 + c3)) + (6 * c2 * c3)) / (12 * (1 - c2) * (1 - c3))
+    elif (case == 2):
+        b3 = alpha[0]
+        c2 = c3 = 1/2
+        c4 = 1
+        a31 = ((3 * b3) - 1) / (6 * b3)
+        a32 = 1 / (6 * b3)
+        a41 = 0
+        a42 = 1 - (3 * b3)
+        a43 = 3 * b3
+        b1 = 1/6
+        b2 = (2 / 3) - b3
+        b4 = 1/6
+    elif (case == 3):
+        b3 = alpha[0]
+        c2 = 1/2
+        c3 = 0
+        c4 = 1
+        a31 = -1 / (12 * b3)
+        a32 = 1 / (12 * b3)
+        a41 = (-1/2) - (6 * b3)
+        a42 = 3/2
+        a43 = 6 * b3
+        b1 = (1/6) - b3
+        b2 = 2/3
+        b4 = 1/6
+    elif (case == 4):
+        b4 = alpha[0]
+        c2 = 1
+        c3 = 1/2
+        c4 = 1
+        a31 = 3/8
+        a32 = 1/8
+        a41 = 1 - (1 / (4 * b4))
+        a42 = -1 / (12 * b4)
+        a43 = 1 / (3 * b4)
+        b1 = 1/6
+        b2 = 1/6 - b4
+        b3 = 2/3
+    elif (case == 5):
+        c2 = alpha[0]
+        c3 = 1/2
+        c4 = 1
+        a31 = ((4 * c2) - 1) / (8 * c2)
+        a32 = 1 / (8 * c2)
+        a41 = (1 - (2 * c2)) / (2 * c2)
+        a42 = -1 / (2 * c2)
+        a43 = 2
+        b1 = 1/6
+        b2 = 0
+        b3 = 2/3
+        b4 = 1/6
 
+    log.info("Coefficients selected for E4 are:")
+    log.info("c2, c3, c4, b1, b2, b3, b4, a31, a32, a41, a42, a43", c2, c3, c4, b1, b2, b3, b4, a31, a32, a41, a42, a43)
     return c2, c3, c4, b1, b2, b3, b4, a31, a32, a41, a42, a43
 
 def E4Eq1(c, b, A):
     #For Equation 1, find b*c^4
     cquad = [c[0] ** 4, c[1] ** 4, c[2] ** 4, c[3] ** 4]
-    bcquad = (b[0] * c[0]) + (b[1] * c[1]) + (b[2] * c[2]) + (b[3] * c[3])
+    bcquad = (b[0] * cquad[0]) + (b[1] * cquad[1]) + (b[2] * cquad[2]) + (b[3] * cquad[3])
 
     return ((1/24) * (bcquad - (1/5)))
 
@@ -175,7 +227,7 @@ def E4Eq4(c, b, A):
            ((A[1][0] * Ac[0]) + (A[1][1] * Ac[1]) + (A[1][2] * Ac[2]) + (A[1][3] * Ac[3])),
            ((A[2][0] * Ac[0]) + (A[2][1] * Ac[1]) + (A[2][2] * Ac[2]) + (A[2][3] * Ac[3])),
            ((A[3][0] * Ac[0]) + (A[3][1] * Ac[1]) + (A[3][2] * Ac[2]) + (A[3][3] * Ac[3]))]
-    bcAAc = (bc[0] + AAc[0]) + (bc[1] + AAc[1]) + (bc[2] + AAc[2]) + (bc[3] + AAc[3])
+    bcAAc = (bc[0] * AAc[0]) + (bc[1] * AAc[1]) + (bc[2] * AAc[2]) + (bc[3] * AAc[3])
 
     return (bcAAc - (1/30))
 
@@ -209,11 +261,11 @@ def E4Eq7(c, b, A):
           ((A[2][0] * c[0]) + (A[2][1] * c[1]) + (A[2][2] * c[2]) + (A[2][3] * c[3])),
           ((A[3][0] * c[0]) + (A[3][1] * c[1]) + (A[3][2] * c[2]) + (A[3][3] * c[3]))]
     cAc = [c[0] * Ac[0], c[1] * Ac[1], c[2] * Ac[2], c[3] * Ac[3]]
-    AcAc = [((A[0][0] * c[0]) + (A[0][1] * c[1]) + (A[0][2] * c[2]) + (A[0][3] * c[3])),
-            ((A[1][0] * c[0]) + (A[1][1] * c[1]) + (A[1][2] * c[2]) + (A[1][3] * c[3])),
-            ((A[2][0] * c[0]) + (A[2][1] * c[1]) + (A[2][2] * c[2]) + (A[2][3] * c[3])),
-            ((A[3][0] * c[0]) + (A[3][1] * c[1]) + (A[3][2] * c[2]) + (A[3][3] * c[3]))]
-    bAcAc = (b[0] * AcAc[0] + b[1] * AcAc[1] + b[2] * AcAc[2] + b[3] * AcAc[3])
+    AcAc = [((A[0][0] * cAc[0]) + (A[0][1] * cAc[1]) + (A[0][2] * cAc[2]) + (A[0][3] * cAc[3])),
+            ((A[1][0] * cAc[0]) + (A[1][1] * cAc[1]) + (A[1][2] * cAc[2]) + (A[1][3] * cAc[3])),
+            ((A[2][0] * cAc[0]) + (A[2][1] * cAc[1]) + (A[2][2] * cAc[2]) + (A[2][3] * cAc[3])),
+            ((A[3][0] * cAc[0]) + (A[3][1] * cAc[1]) + (A[3][2] * cAc[2]) + (A[3][3] * cAc[3]))]
+    bAcAc = (b[0] * AcAc[0]) + (b[1] * AcAc[1]) + (b[2] * AcAc[2]) + (b[3] * AcAc[3])
 
     return (bAcAc - (1/40))
 
@@ -247,16 +299,20 @@ def E4Eq9(c, b, A):
             ((A[2][0] * AAc[0]) + (A[2][1] * AAc[1]) + (A[2][2] * AAc[2]) + (A[2][3] * Ac[3])),
             ((A[3][0] * AAc[0]) + (A[3][1] * AAc[1]) + (A[3][2] * AAc[2]) + (A[3][3] * Ac[3]))]
 
-    bAAAc = (b[0] + AAAc[0]) + (b[1] + AAAc[1]) + (b[2] + AAAc[2]) + (b[3] + AAAc[3])
+    bAAAc = (b[0] * AAAc[0]) + (b[1] * AAAc[1]) + (b[2] * AAAc[2]) + (b[3] * AAAc[3])
 
     return (bAAAc - (1/120))
 
 def E4(alpha):
-    if ((alpha[0] == 0) or (alpha[1] == 0) or (alpha[0] == 1) or (alpha[1] == 1)
-        or (alpha[0] == alpha[1]) or (alpha[0] == 1/2) or ((3 - (4 * (alpha[0] + alpha[1])) + (6 * alpha[0] * alpha[1])) == 0)):
-        return 5
+    if (case == 1):
+        if ((alpha[0] <= 0) or (alpha[1] <= 0) or (alpha[0] == 1) or (alpha[1] == 1)
+            or (alpha[0] == alpha[1]) or (alpha[0] == 1/2) or ((3 - (4 * (alpha[0] + alpha[1])) + (6 * alpha[0] * alpha[1])) == 0)):
+            return 1
+    elif ((case == 2) or (case == 3) or (case == 4) or (case == 5)):
+        if (alpha[0] == 0):
+            return 1
 
-    c2, c3, c4, b1, b2, b3, b4, a31, a32, a41, a42, a43 = setValuesForFourthOrderCase1(alpha)
+    c2, c3, c4, b1, b2, b3, b4, a31, a32, a41, a42, a43 = setValuesForFourthOrder(alpha)
     
     c = [0, c2, c3, c4]
     b = [b1, b2, b3, b4]
@@ -293,18 +349,39 @@ def E4(alpha):
     
     return result
 
-def testE4():
-    alpha = 1/3
-    beta = 2/3
-    c2, c3, c4, b1, b2, b3, b4, a31, a32, a41, a42, a43 = setValuesForFourthOrderCase1([alpha, beta])
+def displayMenu():
+    print ("1. Optimize E2")
+    print ("2. Optimize E3")
+    print ("3. Optimize E4")
+    choice = input("Enter your choice: ")
     
-    c = [0, c2, c3, c4]
-    b = [b1, b2, b3, b4]
-    A = [[0, 0, 0, 0],[c2, 0, 0, 0],[a31, a32, 0, 0], [a41, a42, a43, 0]]
+    return int(choice);
 
-    print (E4Eq5(c, b, A))
+def chooseE4Case():
+    print ("1. Case 1: 0, c2, c3, 1 all distinct,",
+           "\nc2≠1/2 and 3 - 4(c2 + c3) + 6*c2*c3 ≠ 0")
+    print ("2. Case 2: c2 = c3 = 1/2, b3≠0")
+    print ("3. Case 3: c2 = 1/2, c3 = 0, b3≠0")
+    print ("4. Case 4: c2 = 1, c3 = 1/2, b4≠0")
+    print ("5. Case 5: c2≠0, c3 = 1/2, b2 = 0")
+    choice = input("\nEnter your case choice: ")
+    
+    return int(choice)
 
-#testE4()
-optimize(E4)
+def initializeOptimizer(choice):
+    global case
+    if (choice == 1):
+        log.info("Optimizing E2")
+        optimize(E2)
+    elif (choice == 2):
+        log.info("Optimizing E3")
+        optimize(E3)
+    elif (choice == 3):
+        case = chooseE4Case()
+        log.info("Optimizing E4 with case: ", case)
+        optimize(E4)
 
-#print (E3([0.5, 0.75]))
+log = logger.Logger("Optimization Log")
+initializeOptimizer(displayMenu())
+
+del log
