86466e4b89ade8ead2d8e92d476ce3f5ea231ee3
Shivam Singla <singl.shivam01@yahoo.com>
2021-05-25 14:22:47 -0300

    In HermiteInterpolation.py, modified hermite() function to create a long list of t, u, f and d values instead of list of lists for each step. Removed defect() function and added its functionality in hermite() function which now returns the following 4 lists: t, u, f and d. Modified displayResults() function to print all the values from the lists according to the new changes. Added plotHermite() function to plot the graphs for Hermite interpolation and its defect.

--- HermiteInterpolation.py
+++ HermiteInterpolation.py
@@ -57,31 +57,44 @@ Returns:
 '''
 def hermite(tt, yy, ffy):
     h = tt[1] - tt[0]
+    # To store all the t values from start to end
+    t = []
+    # To store the u value computed at (t_i + (theta * h_i)).
+    u = []
+    # To store the derivative of u computed at (t_i + (theta * h_i)).
+    u_d = []
+    # To store the defect at (t_i + (theta * h_i)).
+    d = []
+    # To store all the u values computed
     uu = []
-    ffu = [] #Extra
-    for i in range (0, len(tt) - 1):
-        u_i = []
-        fu_i = [] #Extra
-        for j in range (1, 11):
-            theta = j/10
-            u = []
-            # u_i(t_i + (theta * h_i))
-            for k in range (0, len(yy[i])):
-                u.append((yy[i][k] * h00(theta)) + (h * ffy[i][k] * h10(theta)) + (yy[i + 1][k] * h01(theta)) + (h * ffy[i + 1][k] * h11(theta)))
-            fu = f.formulaExact((tt[i] + (theta * h)), u[:]) #Extra
-            #fu = f.error((tt[i] + (theta * h)), u[:]) #Extra
-            u_i.append(u)
-            fu_i.append(fu) #Extra
-        ffu.append(fu_i)  #Extra
-        uu.append(u_i)
-    return uu, ffu  #Extra
-
-def defect(tt, yy, ffy):
-    h = tt[1] - tt[0]
+    # To store all the exact values at all the t values
+    ffu = []
+    # To store all the defect values in variable 'delta'
     delta = []
 
+    for k in range (0, len(yy[0])):
+        # For the first value, u_0(t_0)
+        u.append((yy[0][k] * h00(0)) + (h * ffy[0][k] * h10(0)) + (yy[1][k] * h01(0)) + (h * ffy[1][k] * h11(0)))
+
+        # For the first derivative value, u_0'(t_0)
+        u_d.append(((yy[0][k] / h) * h00_d(0)) + (ffy[0][k] * h10_d(0)) + ((yy[1][k] / h) * h01_d(0)) + (ffy[1][k] * h11_d(0)))
+    # Here fu = f(t_0, u_0(t_0))
+    fu = f.formula(tt[0], u[:])
+    # Exact value at t_0
+    fe = f.formulaExact(tt[0], u[:])
+
+    # d_0(t_0) = u_0'(t_0) - f(t_0, u_0(t_0))
+    for k in range (0, len(u)):
+        d.append(u_d[k] - fu[k])
+
+    # Storing the values in their corresponding lists
+    t.append(tt[0])
+    uu.append(u)
+    ffu.append(fe)
+    delta.append(d)
+
+    # Performing hermite interpolation on all the intervals
     for i in range (0, len(tt) - 1):
-        d_i = []
         for j in range (1, 11):
             theta = j/10
             u = []
@@ -97,27 +110,77 @@ def defect(tt, yy, ffy):
 
             # Here fu = f(t_i + (theta * h_i), u_i(t_i + (theta * h_i)))
             fu = f.formula((tt[i] + (theta * h)), u[:])
+
+            # Exact value at (t_i + (theta * h_i))
+            fe = f.formulaExact((tt[i] + (theta * h)), u[:])
+
             # d_i(t_i + (theta * h_i)) = u_i'(t_i + (theta * h_i)) - f(t_i + (theta * h_i), u_i(t_i + (theta * h_i)))
             for k in range (0, len(u)):
                 d.append(u_d[k] - fu[k])
-            d_i.append(d)
-        delta.append(d_i)
-    return delta
 
-def displayResults():
-    # For Full test
+            # Storing the values in their corresponding lists
+            t.append(tt[i] + (theta * h))
+            uu.append(u)
+            ffu.append(fe)
+            delta.append(d)
+    return t, uu, ffu, delta
+
+def displayResults(reset=False):
+    # For a full display of hermite interpolant 
     for i in range (0, len(config.t)):
-        u, f = hermite(config.t[i], config.y[i], config.f[i])
-        d = defect(config.t[i], config.y[i], config.f[i])
-        print ("\tu\t\t\tf\t\t\td")
-        for j in range (0, len(u)):
-            print ("Step:", j+1)
-            print ("t =", config.t[i][j])
-            for k in range (0, len(u[i])):
-                print (u[j][k], "\t", f[j][k], "\t", d[j][k])
-    config.t = []
-    config.y = []
-    config.f = []
+        t, u, f, d = hermite(config.t[i], config.y[i], config.f[i])
+        print ("i\t\t\tt\t\t\tu\t\t\tf\t\t\td")
+        for i in range (0, len(t)):
+            print (i+1, "\t", t[i], "\t", u[i], "\t", f[i], "\t", d[i])
+    if(reset):
+        config.t = []
+        config.y = []
+        config.f = []
+
+def plotHermite():
+    t, u, f, d = hermite(config.t[len(config.t)-1], config.y[len(config.y)-1], config.f[len(config.f)-1])
+
+    # Creating lists to prepare them for plotting
+    t_list = []
+    u_list = []
+    f_list = []
+    d_list = []
+    for j in range (0, len(u[0])):
+        t_list.append(t)
+        u_list.append([])
+        f_list.append([])
+        d_list.append([])
+
+    # Preparing the lists for plotting
+    for i in range (0, len(t)):
+        for j in range (0, len(u[i])):
+            u_list[j].append(u[i][j])
+            f_list[j].append(f[i][j])
+            d_list[j].append(d[i][j])
+
+    # Creating an object to create an HTML file for plotting
+    bp.output_file("Plots/Hermite Interpolant.html")
+
+    # Creating a figure to plot in the HTML file
+    p = bp.figure(plot_width = 1366, plot_height = 768)
+
+    # Plotting the data
+    p.multi_line(t_list + t_list, u_list + f_list)
+
+    # Showing the data in the browser
+    bp.show(p)
+
+    # Creating an object to create an HTML file for plotting
+    bp.output_file("Plots/Defect.html")
+
+    # Creating a figure to plot in the HTML file
+    p = bp.figure(plot_width = 1366, plot_height = 768)
+
+    # Plotting the data
+    p.multi_line(t_list, d_list)
+
+    # Showing the data in the browser
+    bp.show(p)
 
 def test():
     print ("h00(0) = " + str(h00(0)) + ", h00(1) = " + str(h00(1)) + ", h00'(0) = " + str(h00_d(0)) + ", h00'(1) = " + str(h00_d(1)))
